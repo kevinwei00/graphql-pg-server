3a321b4777829da7133c37a914356aaa71903ffc
Kevin Wei <chenghaowei@gmail.com>
2020-12-14 18:24:31 -0500

    update graphql-ws implementation

--- src/server.js
+++ src/server.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const express = require('express');
 const http = require('http');
 const cors = require('cors');
+const ws = require('ws');
 const { Client } = require('pg');
 const { ApolloServer, makeExecutableSchema } = require('apollo-server-express');
 const { PostgresPubSub } = require('graphql-postgres-subscriptions');
-const { createServer: createWebSocketServer } = require('graphql-ws');
+const { useServer } = require('graphql-ws/lib/use/ws');
 const { execute, subscribe } = require('graphql');
 const typeDefs = require('./graphql/schema');
 const resolvers = require('./graphql/resolvers');
@@ -30,19 +31,22 @@ const app = express();
 app.use(cors()); 
 apolloServer.applyMiddleware({ app });
 const httpServer = http.createServer(app);
+const wsServer = new ws.Server({
+  server: httpServer,
+  path: '/graphql'
+});
 
 httpServer.listen(PORT, () => {
-  createWebSocketServer(
+  useServer(
     {
       schema,
       execute,
       subscribe
     },
-    {
-      server: httpServer,
-      path: '/graphql'
-    },
+    wsServer
   );
   console.log(`🚀 Server ready at http://localhost:${PORT}${apolloServer.graphqlPath}`)
   console.log(`🚀 Subscriptions ready at ws://localhost:${PORT}${apolloServer.subscriptionsPath}`)
 });
+
+// curl -X POST 'http://localhost:8000/graphql' -H 'Content-Type: application/json' -d '{ "query": "{ messages { text } }" }'
